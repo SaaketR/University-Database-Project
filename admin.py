5e6aba6358db0d81a1b180c97c827d1c546e6b87
Saaket Raman <95548709+SaaketR@users.noreply.github.com>
2023-04-25 13:55:58 -0400

    Updates to accommodate new file names

--- admin.py
+++ admin.py
@@ -8,13 +8,13 @@ All functions related to an admin:
 
 import sqlite3
 
-admin_conn = sqlite3.connect("admin.db")
-admin_cursor = admin_conn.cursor()
+conn = sqlite3.connect("Database.db")
+cursor = conn.cursor()
 
 def main(username, password):      # returns 0 for unsuccessful login and 1 for successful login
     EXIT_SUCCESS = 1
-    admin_cursor.execute("SELECT * FROM admin WHERE username=? AND password=?", (username, password,))
-    query = admin_cursor.fetchall()
+    cursor.execute("SELECT * FROM admin WHERE username=? AND password=?", (username, password,))
+    query = cursor.fetchall()
 
     if (query):     # condition for successful login
         # <--- code for admin menu
